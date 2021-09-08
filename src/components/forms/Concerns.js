c8dff3f130ed9ad5a01da67a3a694f95294868c7
Deepak Mishra <deepakmishra.dm.3323d@gmail.com>
2021-09-08 22:38:03 +0530

    img alignment

--- src/components/forms/Concerns.js
+++ src/components/forms/Concerns.js
@@ -88,7 +88,7 @@ function Concerns() {
   return (
     <Container fluid className="mt-1">
       <Row>
-        <Col sm={12} md={6} lg={6}>
+        <Col sm={12} md={6} lg={6} style={{ marginLeft: "16px" }}>
           {gender === "Male" ? (
             view === "Front View" ? (
               <ImageMapper
